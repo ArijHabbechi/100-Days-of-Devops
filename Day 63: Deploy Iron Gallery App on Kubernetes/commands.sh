bcc4423a9e4a5f99f525dd75780c936858dc05a1
ArijHabbechi <104939804+ArijHabbechi@users.noreply.github.com>
2025-11-02 19:23:50 +0100

    Add Kubernetes deployment commands for Iron Gallery

---
+++ Day 63: Deploy Iron Gallery App on Kubernetes/commands.sh
@@ -0,0 +1,3 @@
+kubectl create namespace iron-namespace-nautilus
+kubectl apply -f iron-gallery-nautilus.yaml -n iron-namespace-nautilus
+kubectl apply -f iron-db-nautilus.yaml-n iron-namespace-nautilus
